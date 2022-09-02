83ff55b2f3abe425766fe284485b8a416a979c28
Sohag <sohaghossain7th@gmail.com>
2022-09-03 02:21:46 +0600

    Single categories loadded by dynamic id

--- js/scriptOfIndex.js
+++ js/scriptOfIndex.js
@@ -11,10 +11,10 @@ const manuNav = async() => {
     const menuNavUl = document.getElementById('menu-nav');
     categories.forEach(categorie => {
         const {category_id,category_name} = categorie;
-        console.log(category_id,category_name);
+        // console.log(category_id,category_name);
         const li = document.createElement('li');
         li.innerHTML = `
-            <a >${category_name}</a>
+            <a onclick="loadNewsByCategories(${category_id},'${category_name}')">${category_name}</a>
         `  
         menuNavDropdownUl.appendChild(li);        
     });
@@ -22,7 +22,7 @@ const manuNav = async() => {
         const {category_id,category_name} = categorie;
         const li = document.createElement('li');
         li.innerHTML = `
-            <a>${category_name}</a>
+            <a onclick="loadNewsByCategories(${category_id},'${category_name}')">${category_name}</a>
         `   
         menuNavUl.appendChild(li);        
     });
@@ -30,3 +30,18 @@ const manuNav = async() => {
 }
 
 manuNav();
+
+const loadNewsByCategoriesId = async(categoryId) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/0${categoryId}`
+    // console.log(url)
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
+    // return data.data.news_category;
+
+}
+const loadNewsByCategories = async(categoryId,categoryName) => {
+    const newsCategory = await loadNewsByCategoriesId(categoryId);
+
+
+}
